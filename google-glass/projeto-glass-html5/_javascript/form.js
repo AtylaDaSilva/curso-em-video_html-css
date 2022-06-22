f09c7629683c286b59f9c13ff2efe8772ed0e0e3
AtylaDaSilva <atylamendes97@gmail.com>
2022-06-21 23:29:39 -0300

    adusted currency format

--- google-glass/projeto-glass-html5/_javascript/form.js
+++ google-glass/projeto-glass-html5/_javascript/form.js
@@ -5,6 +5,6 @@ function calculatePrice() {
     var quant = parseInt(quantBox.value);
     const priceBox = document.querySelector("input#price");
     var totalPrice = quant * 1500; //1500 == price of Google Glass (arbitrary value)
-    if (!totalPrice) totalPrice = "0"; //if totalPrice == NaN
-    priceBox.value = totalPrice;
+    if (!totalPrice) totalPrice = "R$ 0,00"; //if totalPrice == NaN
+    priceBox.value = totalPrice.toLocaleString("pt-BR", {style:"currency", currency:"BRL"});
 }
